97e035e6b4c2b8ff7760166d175a8c324249727a
d-kens <omondikens1@gmail.com>
2024-04-29 15:56:41 +0300

    type guards

--- sort/build/index.js
+++ sort/build/index.js
@@ -7,15 +7,21 @@ class Sorter {
         const { length } = this.collection;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                // if a collection is an array
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // if collection is a string
+                if (typeof this.collection === 'string') {
                 }
             }
         }
     }
 }
-const sorter = new Sorter([10, -9, 3, 0]);
+const sorter = new Sorter([3, 1, 4, 1, 5, 9, 2, 6, 5]);
 sorter.sort();
 console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,24 +1,34 @@
 class Sorter {
-    constructor(public collection: number[]) {}
-
+    constructor(public collection: number[] | string) {}
 
     sort(): void {
-        const { length }  = this.collection;
+        const { length } = this.collection;
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if(this.collection[j] > this.collection[j + 1]) {
-                    const leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j+1];
-                    this.collection[j+1] = leftHand
+
+                // if a collection is an array
+                if (this.collection instanceof Array) {
+                    if(this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+
+                // if collection is a string
+                if(typeof this.collection === 'string') {
+
                 }
             }
         }
     }
 }
 
-
-
-const sorter = new Sorter([10, -9, 3, 0])
-sorter.sort()
+const sorter = new Sorter([3, 1, 4, 1, 5, 9, 2, 6, 5]);
+sorter.sort();
 console.log(sorter.collection);
+
+
+
+
