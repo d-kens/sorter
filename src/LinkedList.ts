db572f06a3d6c5345885f1aa38ff6314fb431a7d
d-kens <omondikens1@gmail.com>
2024-05-03 10:03:49 +0300

    linkedlist implementation

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -62,5 +62,36 @@ export class LinkedList {
         throw new Error('Index out of bounds')
     }
 
+    compare(leftIndex: number, rightIndex: number): boolean {
+        if(!this.head) {
+            throw new Error('List is empty')
+        }
+
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        // This swaps that values only.
+        const leftNode = this.at(leftIndex);
+        const rightNode = this.at(rightIndex);
+
+        const leftHand = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    }
+
+    print(): void {
+        if(!this.head) {
+            return;
+        }
+
+        let current: LinkedListNode | null = this.head;
+
+        while(current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+
 }
 
